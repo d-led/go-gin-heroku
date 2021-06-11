48592537a1f6d52f840429e5a508d4b1204bbdd0
Dmitry Ledentsov <dmlled@yahoo.com>
2021-06-11 10:14:02 +0200

    app version feedback

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server().Run()
@@ -8,10 +12,17 @@ func main() {
 
 func server() *gin.Engine {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hi",
 		})
 	})
+
+	// https://devcenter.heroku.com/changelog-items/630
+	r.GET("/version", func(c *gin.Context) {
+		c.String(200, os.Getenv("SOURCE_VERSION"))
+	})
+
 	return r
 }
--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,12 +15,29 @@ type message struct {
 }
 
 func TestGreeting(t *testing.T) {
-	assert.Equal(t, 4, 2+2)
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-	server().ServeHTTP(response, request)
+	response := get("/")
+	assert.Equal(t, http.StatusOK, response.Result().StatusCode)
 	var m message
 	err := json.NewDecoder(response.Body).Decode(&m)
 	assert.NoError(t, err)
 	assert.Equal(t, "hi", m.Message)
+}
+
+func TestVersion(t *testing.T) {
+	os.Unsetenv("SOURCE_VERSION")
+	response := get("/version")
+	assert.Equal(t, http.StatusOK, response.Result().StatusCode)
+	assert.Equal(t, "", response.Body.String())
+
+	os.Setenv("SOURCE_VERSION", "42")
+	response = get("/version")
+	assert.Equal(t, http.StatusOK, response.Result().StatusCode)
+	assert.Equal(t, "42", response.Body.String())
+}
+
+func get(path string) *httptest.ResponseRecorder {
+	request, _ := http.NewRequest(http.MethodGet, path, nil)
+	response := httptest.NewRecorder()
+	server().ServeHTTP(response, request)
+	return response
 }
