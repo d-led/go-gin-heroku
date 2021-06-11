a620bca7c7baa5e190096748b9c056f36a4b3892
Dmitry Ledentsov <dmlled@yahoo.com>
2021-06-11 09:55:19 +0200

    refactor to testability

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	server().Run()
+}
+
+func server() *gin.Engine {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hi",
 		})
 	})
-	r.Run()
+	return r
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSanity(t *testing.T) {
+type message struct {
+	Message string `json:"message"`
+}
+
+func TestGreeting(t *testing.T) {
 	assert.Equal(t, 4, 2+2)
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+	server().ServeHTTP(response, request)
+	var m message
+	err := json.NewDecoder(response.Body).Decode(&m)
+	assert.NoError(t, err)
+	assert.Equal(t, "hi", m.Message)
 }
